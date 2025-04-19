bfe794a7244a165951a10128e10b8166acf080d5
Nabin Budhathoki <tusharbcbudhathoki@gmail.com>
2025-04-19 18:56:02 +0545

    removed shop by category

--- client/src/pages/shopping-view/home.jsx
+++ client/src/pages/shopping-view/home.jsx
@@ -140,7 +140,7 @@ export const ShoppingHome = () => {
           </div>
         </div>
       </section>
-      <section className=" shopByCategory py-12 bg-gray-50">
+      {/* <section className=" shopByCategory py-12 bg-gray-50">
         <div className="container mx-auto px-4">
           <h2 className="text-3xl font-bold text-center mb-6">
             Shop by category
@@ -167,7 +167,7 @@ export const ShoppingHome = () => {
             ))}
           </div>
         </div>
-      </section>
+      </section> */}
     </div>
   );
 };
